bc7b99066e4d944b987110ae381291ac74748e7c
YadBash <bobby@yadbash.com>
2017-11-01 22:23:50 -0700

    Relocated images to same directory as script
    To use, download and put all files in same folder
    chmod 755 q4os-version.sh 
    
    execute by:  
    ./q4os-version.sh

--- q4os-version.sh
+++ q4os-version.sh
@@ -2,7 +2,9 @@
 
 #Script to display which version Q4OS you are using on your computer
 
-IMAGES_DIR=$HOME/Programming/images
+IMAGE_Orion=./q4os_orion.jpg
+IMAGE_Scorpion=./q4os_scorpion.jpg
+
 version=$(grep -Po '(?<=GNU/Linux )[0-9]+' /etc/issue 2>/dev/null)
 
 yad --title="YadBash.com" --text="Now Checking Q4OS Version" --center --timeout=3 --width=300 --height=300 --no-buttons
@@ -10,15 +12,17 @@ yad --title="YadBash.com" --text="Now Checking Q4OS Version" --center --timeout=
 case "$version" in 
 
 8) 
-IMAGES_DIR=$HOME/Programming/images
-yad --title="You are Running Q4OS" --center --form --columns=1 --no-buttons \
+yad --title="You are Running Q4OS Orion" --center --form --columns=1 --no-buttons \
 --width=300 \
 --height=300 \
---image $IMAGES_DIR/q4os_scorpion.jpg
+--image $IMAGE_Orion
 ;; 
 
 9) 
-yad --text="Q4OS Orion"
+yad --title="You are Running Q4OS Scorpion" --center --form --columns=1 --no-buttons \
+--width=300 \
+--height=300 \
+--image $IMAGE_Scorpion
 ;; 
 
 esac
